8f623cdb1398f543e0b1f1c838b2de0ca1708fe2
sirajuddinrbg11-bit <sirajuddinrbg11@gmail.com>
2026-05-25 07:36:07 -0700

    Update ADVERTISEMENT.py

--- ADVERTISEMENT.py
+++ ADVERTISEMENT.py
@@ -1,19 +1,27 @@
 import streamlit as st
+
+import pickle
+
 import numpy as np
-import joblib
 
-# Load model
-model = joblib.load("lr.sav")
+model=pickle.load(open('lr.sav','rb'))
 
-st.title("Sales Prediction App")
+st.title('Sales Prediction App')
 
-TV = st.number_input("TV Advertising Budget", min_value=0.0)
-Radio = st.number_input("Radio Advertising Budget", min_value=0.0)
-Newspaper = st.number_input("Newspaper Advertising Budget", min_value=0.0)
+# Input Features
 
-if st.button("Predict Sales"):
-    input_data = np.array([[TV, Radio, Newspaper]])
+ 
 
-    prediction = model.predict(input_data)[0]
+TV=st.number_input('TV adv budget', min_value=0.0)
 
-    st.success(f"Predicted Sales: {prediction:.2f}")
+Radio=st.number_input('Radio Adv Budget', min_value=0.0)
+
+Newspaper=st.number_input('newpaper budget', min_value=0.0)
+
+if st.button('predct sales'):
+
+  input_data=np.array([[TV,Radio,Newspaper]])
+
+  Predction=model.predict(input_data)[0]
+
+  st.success(f'predicted Sales: {Predction:.2f}')
